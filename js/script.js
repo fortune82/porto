0dde8abf90f7c08b7ec2d029f98684905a4695db
fortuna8.2 <fortuna8228@gmail.com>
2020-05-28 23:54:35 +0300

    закончил домашку

--- js/script.js
+++ js/script.js
@@ -1,21 +1,10 @@
-// $(document).ready(function () {
-//     $('.portfolio').slick({
-//         infinite: true,
-//         slideToShow: 3,
-//         slideToScroll: 3,
-//         dots: true,
-//         prevArrow: '<img src ="../img/arrow.png">',
-//         nextArrow: '<img src ="../img/arrow.png">'
-//     });
-// });
 $(document).ready(function () {
-    $(".")
-    $('.portfolio').slick({
+    $('.slider').slick({
         infinite: true,
         slideToShow: 3,
         slideToScroll: 3,
-        dots: true,
-        prevArrow: '<img src ="../img/arrow.png">',
-        nextArrow: '<img src ="../img/arrow.png">'
+        dots: false,
+        prevArrow: $('.left'),
+        nextArrow: $('.right')
     });
-});
+})
